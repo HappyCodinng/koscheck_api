ec3e4fa736535b01c640878b1219b8fcb57f94fd
Setsuna Yuki <zyxn.pian@gmail.com>
2026-01-25 13:15:43 +0700

    update check

--- check_db.php
+++ check_db.php
@@ -1,11 +1,25 @@
 <?php
-$conn = new mysqli(
-  getenv("MYSQLHOST"),
-  getenv("MYSQLUSER"),
-  getenv("MYSQLPASSWORD"),
-  getenv("MYSQLDATABASE"),
-  getenv("MYSQLPORT")
-);
+
+ini_set('display_errors', 1);
+ini_set('display_startup_errors', 1);
+error_reporting(E_ALL);
+
+header("Content-Type: text/plain");
+
+echo"Mulai...\n";
+
+$host = getenv("MYSQLHOST");
+$user = getenv("MYSQLUSER");
+$password = getenv("MYSQLPASSWORD");
+$db = getenv("MYSQLDATABASE");
+$port = getenv("MYSQLPORT");
+
+
+var_dump($host, $user, $password, $db, $port);
+
+$conn = new mysqli($host, $user, $password, $db, $port);
+
+echo "Connected\n";
 
 $result = $conn->query("SHOW TABLES");
 
