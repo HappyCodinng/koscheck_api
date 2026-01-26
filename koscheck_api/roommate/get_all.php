fc954689b3b1eaf933e25c65f1815fca07c1fb72
Setsuna Yuki <zyxn.pian@gmail.com>
2026-01-26 10:04:27 +0700

    add Get_all

---
+++ koscheck_api/roommate/get_all.php
@@ -0,0 +1,52 @@
+<?php
+header("Access-Control-Allow-Origin: *");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+header("Access-Control-Allow-Headers: Content-Type, Authorization");
+header("Content-Type: application/json; charset=UTF-8");
+
+if($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+  http_response_code(200);
+  exit;
+}
+
+require_once __DIR__ . "/../config/database.php";
+
+$id_user_login = $_GET['id'] ?? 0;
+$jenis_kelamin = $_GET['jenis_kelamin'] ?? null;
+
+$sql = "SELECT u.id, u.nama,"
+ // u.email, u.no_hp, 
+ "r.umur, r.kampus, r.jenis_kelamin, r.deskripsi 
+        FROM user u
+        JOIN roommate r ON u.id = r.id_user
+        WHERE u.id != ?";
+
+if ($jenis_kelamin && $jenis_kelamin !== "Semua") {
+  $sql .= " AND r.jenis_kelamin = ?";
+  $stmt = $conn->prepare($sql);
+  $stmt->bind_param("is", $id_user_login, $jenis_kelamin);
+} else {
+  $stmt = $conn->prepare($sql);
+  $stmt -> bind_param("i", $id_user_login);
+}
+
+if (!$result) {
+  echo json_encode([
+    "status" => false,
+    "message" => mysqli_error($conn)
+  ]);
+  exit;
+}
+
+$stmt->execute();
+$result = $stmt->get_result();
+
+$data = [];
+while ($row = $result->fetch_assoc()) {
+  $data[] = $row;
+}
+
+echo json_encode([
+  "status" => true,
+  "data" => $data
+]);
