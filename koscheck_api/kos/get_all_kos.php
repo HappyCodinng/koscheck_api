b2ac31d7aa1e793a3a2415e8545d3e66df4ffba9
Setsuna Yuki <zyxn.pian@gmail.com>
2026-02-05 11:57:08 +0700

    Function Filter

--- koscheck_api/kos/get_all_kos.php
+++ koscheck_api/kos/get_all_kos.php
@@ -1,7 +1,26 @@
 <?php
-require_once __DIR__ . '/../config/database.php';
+header('Content-Type: application/json');
+require_once __DIR__ . "/../config/database.php";
 
-$result = $conn->query("SELECT * FROM kos ORDER BY id_kos DESC");
+$search = $_GET['search'] ?? '';
+
+if ($search !== '') {
+    $stmt = $conn->prepare ("
+        SELECT * FROM kos
+        WHERE nama_kos LIKE ?
+        OR alamat LIKE ?
+        ORDER BY Rating DESC
+    ");
+    $keyword = "%$search%";
+    $stmt->bind_param("ss", $keyword, $keyword);
+} else {
+    $stmt = $conn->prepare("
+        SELECT * FROM kos
+        ORDER BY Rating DESC
+    ");
+}
+$stmt->execute();
+$result = $stmt->get_result();
 
 $data = [];
 while ($row = $result->fetch_assoc()) {
